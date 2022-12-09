d62eb0b8cb297ed487f833fe9d7692f77e17b956
Spark Bank <sharmaajit258@gmail.com>
2022-12-09 19:34:59 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var phantom = require('phantom');   
 const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 var expressLayouts = require('express-ejs-layouts');
-let pdf = require("html-pdf");
-let path = require("path");
-var ejs = require('ejs');
-const router = express.Router();
-pdf = require('express-pdf');
-const Student = require("./models/Students");
-const fs = require('fs');
 
 mongoose.connect("mongodb://localhost:27017/task3", {
   useNewUrlParser: true,
@@ -22,6 +14,8 @@ db.once("open", () => console.log("connected"));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+
 app.use(expressLayouts);
 app.set('layout', 'layouts/layout');
 app.use(express.json());
@@ -40,55 +34,7 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-//generating pdf
 
-app.get("/generatepdf", (req, res) => {
-  ejs.renderFile(path.join(__dirname,'./views/student/', "view.ejs"), {students:Student}, (err, data) => {
-  if (err) {
-
-        res.send(err);
-  } else {
-      let options = {
-          "height": "11.25in",
-          "width": "8.5in",
-          "header": {
-              "height": "20mm"
-          },
-          "footer": {
-              "height": "20mm",
-          },
-      };
-      pdf.create(data, options).toFile("report.pdf", function (err, data) {
-          if (err) {
-              res.send(err);
-          } else {
-            
-              res.send("File created successfully");
-          }
-      });
-  }
-});
-})
-
-// phantom.create().then(function(ph) {
-//     ph.createPage().then(function(page) {
-//         page.open("view.ejs",Students).then(function(status) {
-//             page.render('google.pdf').then(function() {
-//                 console.log('Page Rendered');
-//                 ph.exit();
-//             });
-//         });
-//     });
-// });
-app.get('/generatepdf', function (req, res) {
-  var filePath = "views/student/view.ejs";
-
-  fs.readFile(__dirname + filePath , function (err,data){
-      res.contentType("application/pdf");
-      res.send(data);
-  });
-  
-});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
